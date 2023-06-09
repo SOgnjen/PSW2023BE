e1f9cf431adede00a61452a4be14ae8db27d19bf
SOgnjen <srdicognjen@gmail.com>
2023-06-09 18:47:39 +0200

    implemented class User, his enum and tests for it

---
+++ HospitalAPIAppTest/UserTest.cs
@@ -0,0 +1,119 @@
+﻿using HospitalLibrary.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HospitalAPIAppTest
+{
+    public class UserTest
+    {
+        [Fact]
+        public void User_FullModel_ShouldPassValidation()
+        {
+            var user = new User
+            {
+                Id = 1,
+                Emails = "test@example.com",
+                Password = "password123",
+                FirstName = "John",
+                LastName = "Doe",
+                Role = UserRole.ROLE_ADMINISTRATOR,
+                Address = "123 Main St",
+                PhoneNumber = "555-1234",
+                Jmbg = 1234567890,
+                Gender = Gender.MALE,
+                BloodPressure = new List<int> { 120, 80 },
+                BloodSugar = new List<int> { 80, 100 },
+                BodyFat = new List<int> { 25, 30 },
+                Weight = new List<int> { 70, 75 }
+            };
+
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(user);
+            var validationResult = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            var isValid = Validator.TryValidateObject(user, validationContext, validationResult, true);
+
+            Assert.True(isValid);
+            Assert.Empty(validationResult);
+        }
+
+        [Fact]
+        public void User_FullModel_ShouldntPassValidation()
+        {
+            var user = new User
+            {
+                Id = 1,
+                Emails = "testexample.com",
+                Password = "password123",
+                FirstName = "John",
+                LastName = "Doe",
+                Role = UserRole.ROLE_ADMINISTRATOR,
+                Address = "123 Main St",
+                PhoneNumber = "555-1234",
+                Jmbg = 1234567890,
+                Gender = Gender.MALE,
+                BloodPressure = new List<int> { 120, 80 },
+                BloodSugar = new List<int> { 80, 100 },
+                BodyFat = new List<int> { 25, 30 },
+                Weight = new List<int> { 70, 75 }
+            };
+
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(user);
+            var validationResult = new List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            var isValid = Validator.TryValidateObject(user, validationContext, validationResult, true);
+
+            Assert.False(isValid);
+            Assert.NotEmpty(validationResult);
+        }
+
+        [Fact]
+        public void User_MissingRequiredProperties()
+        {
+            var user = new User();
+
+            var validationContext = new ValidationContext(user);
+            var validationResult = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(user, validationContext, validationResult, true);
+
+            Assert.False(isValid);
+            Assert.NotEmpty(validationResult);
+        }
+
+        [Fact]
+        public void User_MaximumStringLengthExceeded()
+        {
+            var user = new User
+            {
+                FirstName = "This is a very long first name that exceeds the maximum length",
+                LastName = "This is a very long last name that exceeds the maximum length"
+            };
+
+            var validationContext = new ValidationContext(user);
+            var validationResult = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(user, validationContext, validationResult, true);
+
+            Assert.False(isValid);
+            Assert.NotEmpty(validationResult);
+        }
+
+        [Fact]
+        public void User_InvalidPhoneNumberFormat()
+        {
+            var user = new User
+            {
+                PhoneNumber = "12345"
+            };
+
+            var validationContext = new ValidationContext(user);
+            var validationResult = new List<ValidationResult>();
+            var isValid = Validator.TryValidateObject(user, validationContext, validationResult, true);
+
+            Assert.False(isValid);
+            Assert.NotEmpty(validationResult);
+        }
+    }
+}
---
+++ src/HospitalLibrary/Core/Model/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Emails { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public string Address { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public int Jmbg { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public List<int> BloodPressure { get; set; }
+
+        public List<int> BloodSugar { get; set; }
+
+        public List<int> BodyFat { get; set; }
+
+        public List<int> Weight { get; set; }
+    }
+}
